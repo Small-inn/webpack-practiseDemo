3c15f5f0337e10e3a0759aed24615b0c2bb01f06
Small_inn <small_inn@163.com>
2019-07-26 14:00:24 +0800

    es6 react 解析

--- my-project/src/search.js
+++ my-project/src/search.js
@@ -1,1 +1,12 @@
 document.write('search')
+
+import React from 'react'
+import ReactDom from 'react-dom'
+
+class Search extends React.Component {
+  render() {
+    return <div>Search Text</div>
+  }
+}
+
+ReactDom.render(<Search />, document.getElementById('root'))
