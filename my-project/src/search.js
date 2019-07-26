8584c90d45a50243bfb055727189505905720fae
Small_inn <small_inn@163.com>
2019-07-26 14:19:18 +0800

    文件图片 解析

--- my-project/src/search.js
+++ my-project/src/search.js
@@ -3,10 +3,15 @@ document.write('search')
 import React from 'react'
 import ReactDom from 'react-dom'
 import './search.css'
+import logo from './images/logo.jpg'
 
 class Search extends React.Component {
   render() {
-    return <div className="search-text">Search Text</div>
+    return (
+      <div className="search-text">
+        Search Text <img src={logo} />
+      </div>
+    )
   }
 }
 
