a9a47e469f1ef0cd90e5f7fa8dcc7eb36b2a95f2
liuxie <small_inn@163.com>
2019-11-30 22:34:45 +0800

    代码分割和动态import，脚本懒加载

--- one-to-end/src/search/index.js
+++ one-to-end/src/search/index.js
@@ -6,10 +6,29 @@ import logo from '../images/logo.png';
 import './search.less';
 
 class Search extends React.Component {
+    constructor() {
+        super(...arguments) 
+
+        this.state = {
+            Text: null
+        }
+    }
+
+    loadComponent() {
+        import('./text.js').then(Text => {
+            this.setState({
+                Text: Text.default
+            })
+        })
+    }
 
     render() {
+        const { Text } = this.state
         return <div className="search-text">
-            搜索文字的内容<img src={ logo } />
+            {
+                Text ? <Text /> : null
+            }
+            搜索文字的内容<img src={ logo } onClick = { this.loadComponent.bind(this) } />
         </div>;
     }
 }
