b59fb990c7124e43051d4fb809556374f33621e5
Small_inn <small_inn@163.com>
2019-12-03 17:46:57 +0800

    构建webpack配置

---
+++ builder-webpack/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "parser": "babel-eslint",
+    "extends": [],
+    "rules": {
+        // "semi": "error",
+        "indent": ["error", 4]
+    },
+    "env": {
+        "browser": true,
+        "node": true
+    }
+}
---
+++ builder-webpack/lib/webpack.base.js
@@ -0,0 +1,132 @@
+const autoprefixer = require('autoprefixer')
+const path = require('path')
+const glob = require('glob')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+const projectRoot = process.cwd()
+
+const setMPA = () => {
+  const entry = {}
+  const htmlWebpackPlugin = []
+  const entryFiles = glob.sync(path.join(__dirname, './src/*/index.js'))
+
+  Object.keys(entryFiles).map(index => {
+    const entryFile = entryFiles[index]
+    const match = entryFile.match(/src\/(.*)\/index\.js/)
+    const pageName = match && match[1]
+    // console.log(pageName)
+    entry[pageName] = entryFile
+    htmlWebpackPlugin.push(
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, `src/${pageName}/index.html`),
+        filename: `${pageName}.html`,
+        chunks: ['vendors', pageName],
+        inject: true,
+        minify: {
+          html5: true,
+          collapseWhitespace: true,
+          preserveLineBreaks: false,
+          minifyCSS: true,
+          minifyJS: true,
+          removeComments: false
+        }
+      })
+    )
+  })
+  // console.log(entryFiles)
+  return {
+    entry,
+    htmlWebpackPlugin
+  }
+}
+
+const { entry, htmlWebpackPlugin } = setMPA()
+
+module.exports = {
+  entry: entry,
+  output: {
+    path: path.join(projectRoot, 'dist'),
+    filename: '[name]_[chunkhash:8].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        use: ['babel-loader', 'eslint-loader']
+      },
+      {
+        test: /.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /.less$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'less-loader',
+          {
+            loader: 'postcss-loader', // autoprefixer、css3自动添加前缀loader
+            options: {
+              plugins: () => [
+                require('autoprefixer')({
+                  browsers: ['last 2 version', '>1%', 'ios 7']
+                })
+              ]
+            }
+          },
+          {
+            loader: 'px2rem-loader', // 尺寸适配兼容loader
+            options: {
+              remUnit: 75, // 基础单位1rem = 75px
+              remPrecision: 8 // 小数点
+            }
+          }
+        ]
+      },
+      {
+        test: /.(png|jpg|gif|jpeg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name]_[hash:8].[ext]'
+            }
+          }
+        ]
+      },
+      {
+        test: /.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name]_[hash:8][ext]'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name]_[contenthash:8].css'
+    }),
+    new CleanWebpackPlugin(), // 自动清除构建产物
+    new FriendlyErrorsWebpackPlugin(), // 优化构建命令日志
+    function() {
+      this.hooks.done.tap('done', stats => {
+        if (
+          stats.compilation.errors &&
+          stats.compilation.errors.length &&
+          process.argv.indexOf('--watch') === -1
+        ) {
+          process.exit(1)
+        }
+      })
+    }
+  ].concat(htmlWebpackPlugin),
+  stats: 'errors-only'
+}
